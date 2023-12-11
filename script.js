2cc5ec9d5dc86a28233cab9624299358ee50d549
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-12-11 00:25:20 -0800

    Add operate to perform one of the base functions

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function multiply(a,b){
 function divide(a,b){
     return a / b;
 }
+
+function operate(a, b, operator){
+    result = 0;
+    if (operator === '+'){
+        result = add(a,b);
+    } else if (operator === '-'){
+        result = subtract(a,b);
+    } else if (operator === '*'){
+        result = multiply(a,b);
+    } else if (operator === '/'){
+        result = divide(a,b);
+    }
+    return result;
+}
+
+let firstNum = 2;
+let secondNum = 3;
+let operator = '+';
+
+console.log(operate(firstNum, secondNum, operator));
+
+
